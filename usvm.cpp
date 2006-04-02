2727616b63ce77c2e0ceb4d667a01e84b835d08b
fabee <fabee@6158dfbb-f704-0410-ac79-e9d9d41a80f0>
2006-04-02 09:37:23 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/fabee/code/universvm@95 6158dfbb-f704-0410-ac79-e9d9d41a80f0

--- usvm.cpp
+++ usvm.cpp
@@ -9,10 +9,11 @@
  *  Includes parts of libsvm.
  *
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This program is free software except for military or military related use; 
+ *  If you don't come under the above exception you can redistribute it 
+ *  and/or modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation; either version 2 of the 
+ *  License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
